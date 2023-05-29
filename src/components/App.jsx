97b20066f78a5717970171dd89416df762c6cea9
IrynaRybka <irarybka@gmail.com>
2023-05-30 01:03:53 +0300

    fix button load more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './HomePage/HomePage';
+import { Home } from '../pages/Home/Home';
+
+const Hero = lazy(() => import('./HeroSearch/HeroSearch').then(module => ({...module, default: module.Hero})));
+
+const HeroesDetails = lazy(() => import('./HeroDetailsComponent/HeroDetailsComponent').then(module => ({...module, default: module.HeroesDetails})));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<Home />} />
+          <Route path="hero" element={<Hero />} />
+          <Route path="hero/:movieId" element={<HeroesDetails />}>
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 };
+
+
